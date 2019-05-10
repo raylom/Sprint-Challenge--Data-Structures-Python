6dd69138c31bc3abd9c84d68b0a06afaa0c68eb4
raylom <rlom96@gmail.com>
2019-05-10 11:42:33 -0500

    completed ef get in ring_buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,22 @@
 class RingBuffer:
-  def __init__(self, capacity):
-    self.capacity = capacity
-    self.current = 0
-    self.storage = [None]*capacity
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None]*capacity
 
-  def append(self, item):
-    pass
+    def append(self, item):
+        # append an element at the end of the buffer
+        self.storage.append(item)
+        if len(self.storage) == self.capacity:
+            self.current
+        pass
 
-  def get(self):
-    pass
+    def get(self):
+        # new array created to add all elements in array that have a value
+        ringArray = []
+
+    # for loop to ensure only values and not value None are added to array
+        for i in self.storage:
+            if i is not None:
+                ringArray.append(i)
+                return ringArray
