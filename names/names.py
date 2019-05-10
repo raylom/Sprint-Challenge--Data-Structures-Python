75d9087f68f1fc063a5f4803e84cf0ca3d2f3f87
raylom <rlom96@gmail.com>
2019-05-10 15:01:35 -0500

    completed names.py section

--- names/names.py
+++ names/names.py
@@ -11,12 +11,13 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             # duplices array will consist of names in both lists
+#             duplicates.append(name_1)
+duplicates = set(names_1) & set(names_2)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
-
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
